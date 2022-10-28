b224a622cd344f122f887e08d4becb9fc0098610
mastainvin <vincent.mastain@outlook.fr>
2022-10-28 14:20:34 +0200

    first commit

---
+++ yougetnicecar/src/main/java/com/jee/yougetnicecar/YougetnicecarApplication.java
@@ -0,0 +1,13 @@
+package com.jee.yougetnicecar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YougetnicecarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YougetnicecarApplication.class, args);
+	}
+
+}
---
+++ yougetnicecar/src/test/java/com/jee/yougetnicecar/YougetnicecarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jee.yougetnicecar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YougetnicecarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
