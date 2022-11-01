9ccdf0db1cde354a806a26851550f4ce962ab9f7
mastainvin <vincent.mastain@outlook.fr>
2022-11-01 19:13:39 +0100

    - accueil controller exemple

--- yougetnicecar/src/main/java/com/jee/yougetnicecar/YougetnicecarApplication.java
+++ yougetnicecar/src/main/java/com/jee/yougetnicecar/YougetnicecarApplication.java
@@ -2,9 +2,17 @@ package com.jee.yougetnicecar;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
-@SpringBootApplication
-public class YougetnicecarApplication {
+@SpringBootApplication(scanBasePackages = "com.jee.yougetnicecar")
+public class YougetnicecarApplication extends SpringBootServletInitializer {
+
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(YougetnicecarApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(YougetnicecarApplication.class, args);
